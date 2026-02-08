3970184d5c4042401812ad36cd7c6ed635277a86
RGAntu <rgantu887@gmail.com>
2026-02-08 20:39:38 +0600

    feat(layout & page): add layout root.jsx & Homepage.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router";
 
+import Home from "../page/Home/Home";
+import Root from "../layout/Root";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children:[
+        {
+            index: true,
+            Component: Home 
+        }
+    ]
   },
 ]);
 
