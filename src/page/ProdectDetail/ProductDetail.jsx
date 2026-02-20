6efccd27689e532fe9ec68ccc61858bc7ab09cbb
RGAntu <rgantu887@gmail.com>
2026-02-20 23:30:30 +0600

    ui(ProductDetail): add more details data

--- src/page/ProdectDetail/ProductDetail.jsx
+++ src/page/ProdectDetail/ProductDetail.jsx
@@ -5,9 +5,9 @@ const ProductDetail = () => {
   const data = useLoaderData();
   const { id } = useParams();
   // console.log(id)
-//   console.log(data);
+  //   console.log(data);
   const singleProduct = data.find((product) => product.id === parseInt(id));
-//   console.log(singleProduct);
+  console.log(singleProduct);
   const {
     image,
     availability,
@@ -26,12 +26,25 @@ const ProductDetail = () => {
     waterResistance,
   } = singleProduct;
   return (
-    <div className="text-center">
-    <h1 className="text-4xl">Name: {brand}</h1>
-      <h2 className="text-xl">Price: ${priceUSD}</h2>
-      <p>Color: {color}</p>
-      <p>Category: {category}</p>
-    
+    <div className="flex flex-col md:flex-row lg:flex-row max-w-7xl mx-auto gap-x-8 my-5 justify-center ">
+      <div className="col-span-1">
+        <img src={image} className="w-[25rem] rounded-lg p-5" alt={id} />
+      </div>
+      <div className="col-span-1 p-5 ">
+        <h1 className="text-3xl">Name: {brand}</h1>
+        <h2 className="text-xl">Price: ${priceUSD}</h2>
+        <p>Color: {color}</p>
+        <p>Category: {category}</p>
+        <p>Quantity: {quantity}</p>
+        <p>Availability: {availability}</p>
+        <p>Movement: {movement}</p>
+        <p className="">Description: {description}</p>
+        <p>Size: {size}</p>
+        <p>Strap: { strap}</p>
+        <p>Type: {type}</p>
+        <p>Water Resistance: {waterResistance}</p>
+        <p>Warranty: {warranty}</p>
+      </div>
     </div>
   );
 };
