ad1622617174f7f76c7d5b04e271b3204b7e6c0f
RGAntu <rgantu887@gmail.com>
2026-02-24 23:57:00 +0600

    feat(index.js): add product to localstorage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
-export const addFavorite = product => {
-console.log(product)
-}
+export const addFavorite = (product) => {
+  const favoriteProduct = JSON.stringify(product);
+//   console.log(product);
+  localStorage.setItem("favorite", favoriteProduct);
+};
